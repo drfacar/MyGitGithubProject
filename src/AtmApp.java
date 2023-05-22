3be447a351192a095f86191736a435f17717ce71
Fatih Acar <drfacar@gmail.com>
2023-05-22 11:10:19 +0300

    v3

--- src/AtmApp.java
+++ src/AtmApp.java
@@ -3,5 +3,6 @@ public class AtmApp {
         System.out.println("Bu bir ATM projesidir.");
         //version2
         System.out.println("Java is easy");
+        System.out.println("Learn java earn money");
     }
 }
